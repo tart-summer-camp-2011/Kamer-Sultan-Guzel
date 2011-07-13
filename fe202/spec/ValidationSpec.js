34624bd6e5658d322bdf6b81e01b1ee9478e5ab4
kamer <zurquessa@gmail.com>
2011-07-13 14:17:58 -0700

    [Validation] Changed validation specifications. Categorized them into three big chunks of validating email, name, surname and zip code. Change expected values from "valid" string to "true" and "invalid" to "false".

--- fe202/spec/ValidationSpec.js
+++ fe202/spec/ValidationSpec.js
@@ -6,85 +6,98 @@
  * To change this template use File | Settings | File Templates.
  */
 
-    describe("Validation Class to validate e-mails, name&surname and zip code formats", function() {
-			var validation;
-            beforeEach(function (){
-				validation = new Validation();
-			});
+describe("Validation Class to validate e-mails, name&surname and zip code formats", function() {
+    var validation;
+    beforeEach(function (){
+	    validation = new Validation();
+	});
 
-			describe("An e-mail like 'osman.yuksel@tart.com.tr' should be validated", function(){
-                it("should return: valid with osman.yuksel@tart.com.tr", function(){
+	describe("Email validation", function(){
+            describe("An e-mail like 'osman.yuksel@tart.com.tr' should be validated", function(){
+                it("should return: true with osman.yuksel@tart.com.tr", function(){
                     var result = validation.isValidEmail("osman.yuksel@tart.com.tr");
-                    expect(result).toEqual("valid");
+                    expect(result).toBeTruthy();
                 });
 
-                it("should return: valid with john_doe@doe.co.uk", function(){
+                it("should return: true with john_doe@doe.co.uk", function(){
                     var result = validation.isValidEmail("john_doe@doe.co.uk");
-                    expect(result).toEqual("valid");
+                    expect(result).toBeTruthy();
                 });
 
             });
 
             describe("An e-mail like 'osman.yuksel@tart' should not be validated", function(){
-                it("should return: invalid with osman.yuksel@tart", function(){
+                it("should return: false with osman.yuksel@tart", function(){
                     var result = validation.isValidEmail("osman.yuksel@tart");
-                    expect(result).toEqual("invalid");
+                    expect(result).toBeFalsy();
                 });
 
             });
 
             describe("An e-mail like 'osman.yüksel@tart.com.tr' should not be validated", function(){
-                it("should return: invalid with osman.yüksel@tart.com.tr", function(){
+                it("should return: false with osman.yüksel@tart.com.tr", function(){
                     var result = validation.isValidEmail("osman.yüksel@tart.com.tr");
-                    expect(result).toEqual("invalid");
+                    expect(result).toBeFalsy();
                 });
 
 
             });
+    });
 
+    describe("Name and surname validation", function(){
             describe("A name and surname longer than 30 characters should not be validated", function(){
-                it("should return: invalid with Osman Yüksel Osman Yüksel Osman Yüksel Osman Yüksel", function(){
+                it("should return: false with Osman Yüksel Osman Yüksel Osman Yüksel Osman Yüksel", function(){
                     var result = validation.isValidNameSurname("Osman Yüksel Osman Yüksel Osman Yüksel Osman Yüksel");
-                    expect(result).toEqual("invalid");
+                    expect(result).toBeFalsy();
                 });
              });
 
             describe("A name and surname with alpha numeric characters should be validated", function(){
-                it("should return: valid with Osman Yüksel", function(){
+                it("should return: true with Osman Yüksel", function(){
                     var result = validation.isValidNameSurname("Osman Yüksel");
-                    expect(result).toEqual("valid");
+                    expect(result).toBeTruthy();
                 });
 
-                 it("should return: valid with Osman Yüksel Yüksel", function(){
+                 it("should return: true with Osman Yüksel Yüksel", function(){
                     var result = validation.isValidNameSurname("Osman Yüksel Yüksel");
-                    expect(result).toEqual("valid");
+                    expect(result).toBeTruthy();
                 });
             });
+
+             describe("Single word entries should not be validated", function(){
+                it("should return: false with Osman", function(){
+                    var result = validation.isValidNameSurname("Osman ");
+                    expect(result).toBeFalsy();
+                });
+             });
+
 
             describe("A name and surname with non letter  characters should not be validated", function(){
-                it("should return: invalid with Osman.Yüksel", function(){
+                it("should return: false with Osman.Yüksel", function(){
                     var result = validation.isValidNameSurname("Osman.Yüksel");
-                    expect(result).toEqual("invalid");
+                    expect(result).toBeFalsy();
                 });
 
-                  it("should return: invalid with empty string", function(){
+                  it("should return: false with empty string", function(){
                     var result = validation.isValidNameSurname("");
-                    expect(result).toEqual("invalid");
+                    expect(result).toBeFalsy();
                 });
             });
+    });
 
+     describe("Zip code validation", function(){
 
             describe("A zip code having non numeric characters should not be validated", function(){
-                it("should return: invalid with abcde", function(){
+                it("should return: false with abcde", function(){
                     var result = validation.isValidZipCode("abcde");
-                    expect(result).toEqual("invalid");
+                    expect(result).toBeFalsy();
                 });
             });
 
             describe("A zip code with a space character should not be validated", function(){
-                it("should return: invalid with 12 34", function(){
+                it("should return: false with 12 34", function(){
                     var result = validation.isValidZipCode("12 34");
-                    expect(result).toEqual("invalid");
+                    expect(result).toBeFalsy();
                 });
 
             });
@@ -92,12 +105,13 @@
 
 
             describe("A zip code with 5 numeric characters should be validated", function(){
-                it("should return: valid with 12345", function(){
+                it("should return: true with 12345", function(){
                     var result = validation.isValidZipCode("12345");
-                    expect(result).toEqual("valid");
+                    expect(result).toBeTruthy();
                 });
             });
+     });
 
 
-		});
+});
 
